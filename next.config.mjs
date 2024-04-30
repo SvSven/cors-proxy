e2f3bcf65036b0abbfa490ceccc8472ebd986abb
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-04-30 10:54:22 +0200

    🔧 add cors config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this with actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
