2b9157612f209f5830b6de13910503e89059dcce
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-04-30 10:54:58 +0200

    🚧 set up route to get weather forecast

--- app/api/forecast/route.ts
+++ app/api/forecast/route.ts
@@ -1,6 +1,50 @@
+import { NextResponse, NextRequest } from "next/server";
+
 export const dynamic = "force-dynamic"; // static by default, unless reading the request
 export const runtime = "nodejs";
 
-export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+const apiHeaders = new Headers({
+  Accept: "application/json",
+  "Content-Type": "application/json",
+  "User-Agent": "WeatherAppPrototype github.com/SvSven",
+});
+
+const getForecast = async (lat: string, lng: string) => {
+  const result = await fetch(
+    `https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=${lat}&lon=${lng}`,
+    {
+      method: "GET",
+      headers: apiHeaders,
+    }
+  );
+
+  if (result) {
+    const forecast = await result.json();
+    return forecast;
+  }
+
+  return false;
+};
+
+export async function GET(request: NextRequest) {
+  const lat = request.nextUrl.searchParams.get("lat");
+  const lng = request.nextUrl.searchParams.get("lng");
+
+  if (!lat || !lng) {
+    return NextResponse.json(
+      { error: "Missing search parameters" },
+      { status: 400 }
+    );
+  }
+
+  const forecast = await getForecast(lat, lng);
+
+  if (!forecast) {
+    return NextResponse.json(
+      { error: "An error occured while fetching forecast" },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({ forecast: forecast });
 }
