037721208aa488175c3c571259de682a8860ccc4
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-05-23 11:04:53 +0200

    ⚗️ attempt to set cache for forecast

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,15 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: "/api/forecast",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=59",
+          },
+        ],
+      },
     ];
   },
 };
